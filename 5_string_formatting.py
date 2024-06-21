aa7cb00ed8bf0a8ed4293deadb4f0012e2e3b262
DVN111 <asus.daniyal@gmail.com>
2024-06-21 14:12:55 +0700

    day 3

---
+++ 5_string_formatting.py
@@ -0,0 +1,8 @@
+ini_string = "kata"
+ini_integer = 10
+ini_float = 2.50
+
+data = (ini_string,ini_integer,ini_float)
+
+print("Ini adalah string (%s)\nini adalah integer (%d)\nini adalah float dengan 2 digit dibelakang koma (%.2f)" % data)
+print(f"Ini adalah string ({ini_string})\nini adalah integer ({ini_integer})\nini adalah float dengan 2 digit dibelakang koma ({ini_float})")
